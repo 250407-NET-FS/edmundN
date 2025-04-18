298010aa4713e7a52d75b182b1cb01bef4827bc0
beemanz134 <beeman134@gmail.com>
2025-04-18 02:32:00 -0500

    added xunit working basic test

---
+++ test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace test;
+using Xunit;
+
+public class UnitTest1
+{
+    public class EndpointTests
+    {
+        [Fact]
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+        {
+            // Arrange
+            int number1 = 5;
+            int number2 = 3;
+            int expectedSum = 8;
+
+            // Act
+            int actualSum = Add(number1, number2);
+
+            // Assert
+            Assert.NotEqual(expectedSum, actualSum);
+
+        }
+
+        private int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
